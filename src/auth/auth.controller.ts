dfbb700986f514ec5311f4d651f3f55df1c29e14
MN70Z <m.kattan@bab.com.sa>
2024-11-23 00:44:40 +0300

    refactor: remove unused test files and improve code formatting in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,14 +3,14 @@ import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post() // Sub-route
-    async register(@Body() body: { name: string; email: string; password: string }) {
-      console.log('Register endpoint hit', body); // Debugging log
-      const { name, email, password } = body;
-      return this.authService.register(name, email, password);
-    }
-    
-    
+  @Post() // Sub-route
+  async register(
+    @Body() body: { name: string; email: string; password: string },
+  ) {
+    console.log('Register endpoint hit', body); // Debugging log
+    const { name, email, password } = body;
+    return this.authService.register(name, email, password);
+  }
 }
