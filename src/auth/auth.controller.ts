7026e3b3da419c782f403cab2e75eee6cd70c689
rayanekbr <147691568+rayanekbr@users.noreply.github.com>
2024-11-25 04:08:15 -0800

    rrrrr

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
-    @Post() // Sub-route
+    @Post('register') // Sub-route
     async register(@Body() body: { name: string; email: string; password: string }) {
       console.log('Register endpoint hit', body); // Debugging log
       const { name, email, password } = body;
       return this.authService.register(name, email, password);
     }
     
+    @Post('login')
+  async login(
+    @Body() body: { email: string; password: string }
+  ): Promise<{ accessToken: string }> {
+    const { email, password } = body;
+
+    if (!email || !password) {
+      throw new BadRequestException('Email and password are required');
+    }
+
+    return this.authService.login(email, password);
+  }
     
 }
